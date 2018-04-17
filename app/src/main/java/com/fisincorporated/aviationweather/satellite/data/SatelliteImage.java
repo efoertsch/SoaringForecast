27d289529cffa7f6c6e8eb00b5cf3651674b216f
Eric Foertsch <efoertsch@fisincorporated.com>
2018-04-17 09:50:44 -0400

    WIP.

---
+++ app/src/main/java/com/fisincorporated/aviationweather/satellite/data/SatelliteImage.java
@@ -0,0 +1,12 @@
+package com.fisincorporated.aviationweather.satellite.data;
+
+import com.fisincorporated.aviationweather.common.BitmapImage;
+
+
+public class SatelliteImage extends BitmapImage {
+
+    public SatelliteImage(String imageName) {
+        super(imageName);
+    }
+
+}
