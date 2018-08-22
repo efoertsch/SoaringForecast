53c953ab49e3b69e80da1c920cdb9cc51c5571fb
Eric Foertsch <efoertsch@fisincorporated.com>
2018-08-21 20:40:26 -0400

    Fixed drag and drop with swipe to delete. Click on airport search item to add to list. Some code cleanup.

--- app/src/main/java/com/fisincorporated/aviationweather/airport/list/AirportListAdapter.java
+++ app/src/main/java/com/fisincorporated/aviationweather/airport/list/AirportListAdapter.java
@@ -2,10 +2,10 @@ package com.fisincorporated.aviationweather.airport.list;
 
 import android.databinding.DataBindingUtil;
 import android.graphics.Color;
-import android.support.v4.view.MotionEventCompat;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 
 import com.fisincorporated.aviationweather.R;
@@ -18,16 +18,22 @@ import com.fisincorporated.aviationweather.touchhelper.OnStartDragListener;
 import java.util.Collections;
 import java.util.List;
 
+// !!! Note that in order to get swipe to work needed to put views inside RelativeLayout rather than
+// !!! just having them in ConstraintLayout
 public class AirportListAdapter extends RecyclerView.Adapter<AirportListAdapter.BindingHolder>
         implements ItemTouchHelperAdapter {
 
     private List<Airport> airports;
     private AirportListAdapter.OnItemClickListener onItemClickListener;
-
     private OnStartDragListener mDragStartListener;
+    private NewAirportListListener newAirportListListener;
 
     public interface OnItemClickListener {
         void onItemClick(Airport airport);
+    }
+
+    public interface NewAirportListListener {
+        void newAirportOrder(List<Airport> airports);
     }
 
     public AirportListAdapter() {
@@ -40,6 +46,11 @@ public class AirportListAdapter extends RecyclerView.Adapter<AirportListAdapter.
 
     public AirportListAdapter setOnStartDragListener(OnStartDragListener dragStartListener) {
         mDragStartListener = dragStartListener;
+        return this;
+    }
+
+    public AirportListAdapter setNewAirportListListener(NewAirportListListener newAirportListListener){
+        this.newAirportListListener = newAirportListListener;
         return this;
     }
 
@@ -62,7 +73,7 @@ public class AirportListAdapter extends RecyclerView.Adapter<AirportListAdapter.
         if (mDragStartListener != null) {
             // Start a drag whenever the handle view it touched
             holder.itemView.setOnTouchListener((v, event) -> {
-                if (MotionEventCompat.getActionMasked(event) == MotionEvent.ACTION_DOWN) {
+                if (event.getAction() == MotionEvent.ACTION_DOWN) {
                     mDragStartListener.onStartDrag(holder);
                 }
                 return false;
@@ -70,8 +81,12 @@ public class AirportListAdapter extends RecyclerView.Adapter<AirportListAdapter.
         }
 
         if (onItemClickListener != null) {
-            holder.itemView.setOnClickListener(v -> {
-                onItemClickListener.onItemClick(holder.binding.getAirport());
+            // Not sure why holder.itemView.setOnClickListner didn't work
+            holder.itemView.findViewById(R.id.airport_layout_view).setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    onItemClickListener.onItemClick(holder.binding.getAirport());
+                }
             });
         }
     }
@@ -81,13 +96,21 @@ public class AirportListAdapter extends RecyclerView.Adapter<AirportListAdapter.
     public void onItemDismiss(int position) {
         airports.remove(position);
         notifyItemRemoved(position);
+        notifyNewAirportList(airports);
     }
 
     @Override
     public boolean onItemMove(int fromPosition, int toPosition) {
         Collections.swap(airports, fromPosition, toPosition);
         notifyItemMoved(fromPosition, toPosition);
+        notifyNewAirportList(airports);
         return true;
+    }
+
+    private void notifyNewAirportList(List<Airport> airports) {
+        if (newAirportListListener != null){
+            newAirportListListener.newAirportOrder(airports);
+        }
     }
 
 
