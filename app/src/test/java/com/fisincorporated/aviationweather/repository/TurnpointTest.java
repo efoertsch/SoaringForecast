35d2d9921b9d1076bb633114cf69640bbf635f35
Eric Foertsch <efoertsch@fisincorporated.com>
2018-09-26 22:54:47 -0400

    Started adding turnpoint database

---
+++ app/src/test/java/com/fisincorporated/aviationweather/repository/TurnpointTest.java
@@ -0,0 +1,48 @@
+package com.fisincorporated.aviationweather.repository;
+
+import org.junit.Test;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertNotNull;
+
+public class TurnpointTest {
+
+    String turnpointString = "\"Sterling\",\"3B3\",US,4225.500N,07147.470W,459ft,5,160,3086ft,122.900,\"Home Field, Finish Point, Turn Point, 3B3, RW width: 40, CTAF: 122.9, Fuel: 100LL\"";
+
+    @Test
+    public void convertFromStringToTurnpointTest() throws Exception {
+        Turnpoint turnpoint = Turnpoint.createTurnpointFromCSVDetail(turnpointString);
+        assertNotNull(turnpoint);
+        assertEquals("Incorrect title", "Sterling",turnpoint.getTitle());
+        assertEquals("Incorrect code", "3B3",turnpoint.getCode());
+    }
+
+    @Test
+    public void northLatitudeConversionTest() throws Exception {
+        float latitude = Turnpoint.convertToLat("4225.500N");
+        String convertedLatString = String.format("%.5f", latitude);
+        assertEquals(" Latitude not converted correctly", "42.42500", convertedLatString );
+    }
+
+    @Test
+    public void southLatitudeConversionTest() throws Exception {
+        float latitude = Turnpoint.convertToLat("4225.500S");
+        String convertedLatString = String.format("%.5f", latitude);
+        assertEquals(" Latitude not converted correctly", "-42.42500", convertedLatString );
+    }
+
+
+    @Test
+    public void westLongitudeConversionTest() throws Exception {
+        float longitude = Turnpoint.convertToLong("07147.470W");
+        String convertedLongString = String.format("%.6f", longitude);
+        assertEquals(" longitude not converted correctly", "-71.791168", convertedLongString );
+    }
+
+    @Test
+    public void eastLongitudeConversionTest() throws Exception {
+        float longitude = Turnpoint.convertToLong("07147.470E");
+        String convertedLongString = String.format("%.6f", longitude);
+        assertEquals(" longitude not converted correctly", "71.791168", convertedLongString );
+    }
+}
