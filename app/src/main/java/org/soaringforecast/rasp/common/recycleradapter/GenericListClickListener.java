6553331059c21bb5fe2bef723c2bb70fea5e19ea
Eric Foertsch <efoertsch@fisincorporated.com>
2020-05-15 15:07:51 -0400

    Start of turnpoint list alphabetic index and code cleanup

--- app/src/main/java/org/soaringforecast/rasp/common/recycleradapter/GenericListClickListener.java
+++ app/src/main/java/org/soaringforecast/rasp/common/recycleradapter/GenericListClickListener.java
@@ -1,5 +1,6 @@
 package org.soaringforecast.rasp.common.recycleradapter;
 
+// TODO - don't think I need position in listener - check and remove
 public interface GenericListClickListener<T> {
 
     void onItemClick(T object, int position);
